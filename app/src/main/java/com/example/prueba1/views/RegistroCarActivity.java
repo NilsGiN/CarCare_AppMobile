1d050437dab771883c8f1b524e1010cb90d58582
NilsGiN <nilsgiles200200@gmail.com>
2024-10-16 12:38:49 -0500

    Agregar registro de auto a la BD

--- app/src/main/java/com/example/prueba1/views/RegistroCarActivity.java
+++ app/src/main/java/com/example/prueba1/views/RegistroCarActivity.java
@@ -2,19 +2,42 @@ package com.example.prueba1.views;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.prueba1.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistroCarActivity extends AppCompatActivity {
     private ImageButton atras_main;
+    private Button btnRegistrar;
+
+    private EditText inputmarca;
+    private EditText inputmodelo;
+    private EditText inputplaca;
+    private EditText inputanio;
+    private EditText inputsistema;
+
+    private FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +55,81 @@ public class RegistroCarActivity extends AppCompatActivity {
             }
         });
 
+        // Para BD
+        firestore = FirebaseFirestore.getInstance();
+
+        inputmarca = findViewById(R.id.Marca);
+        inputmodelo = findViewById(R.id.Modelo);
+        inputplaca = findViewById(R.id.Placa);
+        inputanio = findViewById(R.id.Anio);
+        inputsistema = findViewById(R.id.Sistema);
+
+        inputplaca.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Convertimos el texto a mayúsculas
+                String upperCaseText = s.toString().toUpperCase();
+
+                // Evitamos que el TextWatcher se vuelva a disparar si el texto ya está en mayúsculas
+                if (!upperCaseText.equals(s.toString())) {
+                    inputplaca.setText(upperCaseText);
+                    inputplaca.setSelection(upperCaseText.length());  // Colocamos el cursor al final del texto
+                }
+            }
+        });
+
+        btnRegistrar = findViewById(R.id.BtnRegistrar);
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String marca = inputmarca.getText().toString();
+                String modelo = inputmodelo.getText().toString();
+                String placa = inputplaca.getText().toString();
+                String anio = inputanio.getText().toString();
+                String sistema = inputsistema.getText().toString();
+                if(marca.isEmpty() || modelo.isEmpty() || placa.isEmpty() || anio.isEmpty() || sistema.isEmpty()){
+                    Toast.makeText(RegistroCarActivity.this, "Todos los campos son obligatorios",Toast.LENGTH_SHORT).show();
+                }else{
+                    Map<String,Object> CarMap = new HashMap<>();
+                    CarMap.put("marca",marca);
+                    CarMap.put("modelo",modelo);
+                    CarMap.put("placa",placa);
+                    CarMap.put("anio",anio);
+                    CarMap.put("sistema",sistema);
+                    /* taskMap.put("due",dueDate);
+                    taskMap.put("time",time);
+                    taskMap.put("status",0); */
+
+                    firestore.collection("Vehiculo").add(CarMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentReference> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(RegistroCarActivity.this,"Registro de auto guardado", Toast.LENGTH_SHORT).show();
+                                finish(); // Cierra la actividad después de guardar
+                            }else{
+                                //Toast.makeText(RegistroMantenimientoActivity.this,task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                String errorMessage = task.getException() != null ? task.getException().getMessage() : "Error desconocido";
+                                Toast.makeText(RegistroCarActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(RegistroCarActivity.this,e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
